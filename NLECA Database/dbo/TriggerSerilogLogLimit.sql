9967b4171cd3bcca4d88cd0b6fcb0d4613927be0
jloiacano <j.loiacano55@gmail.com>
2020-05-06 10:15:12 -0500

    Added trigger for SerilogLog to limit the number of log entries to 50

---
+++ NLECA Database/dbo/TriggerSerilogLogLimit.sql
@@ -0,0 +1,10 @@
+﻿CREATE TRIGGER [TriggerSerilogLogLimit]
+ON [dbo].[SerilogLog]
+AFTER INSERT
+AS
+    SET NOCOUNT ON;
+	
+    IF (SELECT COUNT(*) FROM [dbo].[SerilogLog]) > 50
+		BEGIN
+			DELETE FROM [dbo].[SerilogLog] where TimeStamp IN (SELECT TOP 1 TimeStamp FROM [dbo].[SerilogLog] ORDER BY TimeStamp ASC);
+		END
