e919537dacdc673f107b3537d32cdf9dabd8e347
jloiacano <j.loiacano55@gmail.com>
2020-05-20 20:14:56 -0500

    Added stored procedure UpdateArticleSequence

---
+++ NLECA Database/dbo/Stored Procedures/Article/UpdateArticleSequence.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateArticleSequence]
+	@newsletterId INT,
+	@oldArticleSequence INT,
+	@updatedArticleSequence INT,
+	@totalRowsEffected INT OUTPUT
+AS
+
+SET NOCOUNT OFF
+
+	UPDATE Articles
+
+	SET ArticleSequence = @updatedArticleSequence
+
+	WHERE NewsletterId = @newsletterId AND ArticleSequence = @oldArticleSequence;
+
+	SET @totalRowsEffected = @@ROWCOUNT
