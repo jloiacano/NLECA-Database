957d3f1045c2888e8059bffbf6f2d3d5c30d6572
jloiacano <j.loiacano55@gmail.com>
2020-05-27 23:04:37 -0500

    added stored procedures for ArticleImages

---
+++ NLECA Database/dbo/Stored Procedures/Article/AddImageToArticleImages.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[AddImageToArticleImages]
+	@articleId int = 0,
+	@md5Hash VARCHAR(50),
+	@imageName VARCHAR(50),
+	@imageLocation VARCHAR(200)
+AS
+
+BEGIN
+	DECLARE @rowsEffected INT = 0;
+
+	INSERT INTO ArticleImages (ArticleId, Md5Hash, ImageName, ImageLocation)
+	VALUES (@articleId, @md5Hash, @imageName, @imageLocation);
+
+	SET @rowsEffected = @@ROWCOUNT;
+
+	SELECT @rowsEffected;
+END
+
+
+GRANT EXECUTE ON OBJECT::[dbo].[AddImageToArticleImages] TO nlecaApp; 
+GO
+
+
---
+++ NLECA Database/dbo/Stored Procedures/Article/CheckIfArticleImageExists.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfArticleImageExists]
+	@md5 VARCHAR(50)
+AS
+BEGIN
+	SELECT * FROM ArticleImages WHERE Md5Hash = @md5;
+END
+GO
+	
+GRANT EXECUTE ON OBJECT::[dbo].[CheckIfArticleImageExists] TO nlecaApp; 
+GO
