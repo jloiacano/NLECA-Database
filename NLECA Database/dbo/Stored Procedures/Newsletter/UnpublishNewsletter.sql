ce4f51b8ce4b3674ffce7d7934282a17f15b3c53
jloiacano <j.loiacano55@gmail.com>
2020-06-05 12:50:56 -0500

    Added UnpublishNewsletter stored procedure

---
+++ NLECA Database/dbo/Stored Procedures/Newsletter/UnpublishNewsletter.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[UnpublishNewsletter]
+		@newsletterId INT
+AS
+	DECLARE @rowsEffected INT;
+
+	BEGIN
+		UPDATE Newsletters
+		SET IsCurrent = 0
+		WHERE NewsletterId = @newsletterId;
+
+		SET @rowsEffected = @@ROWCOUNT;
+	END
+
+	SELECT @rowsEffected;
+
+GRANT EXECUTE ON OBJECT::[dbo].[UnpublishNewsletter] TO nlecaApp; 
+GO
