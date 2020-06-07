4d9af3886054dd9494e1e8d8aa120e270c61949a
jloiacano <j.loiacano55@gmail.com>
2020-06-07 11:21:15 -0500

    Added UnpublishEvent stored procedure

---
+++ NLECA Database/dbo/Stored Procedures/Events/UnpublishEvent.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[UnpublishEvent]
+	@eventId INT
+AS
+		UPDATE Events
+		SET IsPublished = 0
+		WHERE EventId = @eventId;
+
+		SELECT @@ROWCOUNT;
+
+GRANT EXECUTE ON OBJECT::[dbo].[UnpublishEvent] TO nlecaApp; 
+GO
+
