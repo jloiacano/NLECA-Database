eed4f24431dbc9c50cc17a245566a2f677c95a49
jloiacano <j.loiacano55@gmail.com>
2020-06-08 13:34:16 -0500

    Added GetAllCurrentAlerts stored procedure

---
+++ NLECA Database/dbo/Stored Procedures/Alert/GetAllCurrentAlerts.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetAllCurrentAlerts]
+AS
+
+DECLARE @now DATETIME = GETDATE();
+
+	SELECT * FROM Alerts
+	WHERE IsPublished = 1
+		AND @now BETWEEN AlertDate AND AlertDateEnd;
+
+GRANT EXECUTE ON OBJECT::[dbo].[GetAllCurrentAlerts] TO nlecaApp; 
+GO
